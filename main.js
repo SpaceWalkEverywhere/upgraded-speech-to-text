25c5eb7b4a946476f127e531b5ca096e26128b2f
SpaceWalkEverywhere <74199628+SpaceWalkEverywhere@users.noreply.github.com>
2021-04-30 16:52:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function snap(){
         document.getElementById("snapshot").innerHTML='<img src="'+data_URI+'" id="res_img" class="img-responsive">';
     })
 }
-classify=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZJk26a4ld/model.json',modell);
+classify=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UNZBt7j6L/model.json',modell);
 function modell(){
     console.log("Wooooohooooo! Model Loaded")
 }
@@ -24,4 +24,39 @@ function speak(){
     pls_utter= new SpeechSynthesisUtterance(predsp1+predsp2);
     speakk.speak(pls_utter);
 }
-cameraaa="webcam starrrrrrrrrrrrrrrrt"
+function check() {
+    img_saved=document.getElementById("res_img");
+    classify.classify(img_saved,gr);
+}
+function gr(error,result) {
+    if (error) {
+        console.error("errrrorrrr !!! Malfunctionnnnn");
+    }
+    else {
+        console.log(result);
+        document.getElementById("en1").innerHTML=result[0].label;
+        document.getElementById("en2").innerHTML=result[1].label;
+        pr1=result[0].label;
+        pr2=result[1].label;
+        speak()
+        if(pr1=="Happy"){
+            document.getElementById("upd_emg1").innerHTML="<span>&#128514</span>";
+        }
+        else if(pr1=="Sad"){
+            document.getElementById("upd_emg1").innerHTML="&#128550";
+        }
+        else if(pr1=="Angry"){
+            document.getElementById("upd_emg1").innerHTML="&#128545";
+        }
+        if(pr2=="Happy"){
+            document.getElementById("upd_emg2").innerHTML="&#128514";
+        }
+        else if(pr2=="Sad"){
+            document.getElementById("upd_emg2").innerHTML="&#128550";
+        }
+        else if(pr2=="Angry"){
+            document.getElementById("upd_emg2").innerHTML="&#128545";
+        }
+    }
+}
+
